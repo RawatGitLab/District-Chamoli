726abc8436138fdb08c77902fa1aeaab4b986dd5
Varun Rawat <varunrawatgithub2507@gmail.com>
2026-07-01 11:59:13 +0530

    This is first commit.

---
+++ check-properties.js
@@ -0,0 +1,73 @@
+import { MongoClient } from "mongodb";
+
+const MONGODB_URI = "mongodb+srv://varunrawatmailbox2507_db_user:GYVPiF8LG4HIbsSF@cluster0.8xfepsq.mongodb.net/?appName=Cluster0";
+const MONGODB_DB = "Shapefile";
+const MONGODB_COLLECTION = "Chamoli";
+
+async function run() {
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  const db = client.db(MONGODB_DB);
+  const collection = db.collection(MONGODB_COLLECTION);
+  
+  const docs = await collection.find({}).toArray();
+  console.log("Total docs:", docs.length);
+  
+  docs.forEach((doc) => {
+    const layerName = doc.name || doc.Layer || doc.layer || "Unassigned";
+    let featureCount = 0;
+    let sampleGeom = null;
+    let sampleProps = null;
+    
+    let coordSamples = [];
+    let xMin = Infinity, xMax = -Infinity, yMin = Infinity, yMax = -Infinity;
+    
+    function processCoords(coords) {
+      if (!Array.isArray(coords)) return;
+      if (coords.length === 2 && typeof coords[0] === 'number') {
+        const [x, y] = coords;
+        if (isFinite(x) && isFinite(y)) {
+          if (x < xMin) xMin = x;
+          if (x > xMax) xMax = x;
+          if (y < yMin) yMin = y;
+          if (y > yMax) yMax = y;
+          if (coordSamples.length < 5) coordSamples.push([x, y]);
+        }
+      } else {
+        coords.forEach(processCoords);
+      }
+    }
+
+    if (Array.isArray(doc.features)) {
+      featureCount = doc.features.length;
+      if (featureCount > 0) {
+        sampleGeom = doc.features[0].geometry?.type;
+        sampleProps = doc.features[0].properties;
+        doc.features.forEach(f => {
+          if (f.geometry?.coordinates) {
+            processCoords(f.geometry.coordinates);
+          }
+        });
+      }
+    } else if (doc.geometry) {
+      featureCount = 1;
+      sampleGeom = doc.geometry.type;
+      sampleProps = doc.properties || doc;
+      if (doc.geometry.coordinates) {
+        processCoords(doc.geometry.coordinates);
+      }
+    }
+    
+    console.log(`Layer: "${layerName}" | count: ${featureCount} | type: ${sampleGeom}`);
+    if (xMin !== Infinity) {
+      console.log(`   Bounding Box: X [${xMin}, ${xMax}] | Y [${yMin}, ${yMax}]`);
+      console.log(`   Sample Raw Coordinates: ${JSON.stringify(coordSamples)}`);
+    }
+    if (sampleProps) {
+      console.log(`   Sample Properties: ${JSON.stringify(Object.keys(sampleProps))}`);
+    }
+  });
+  
+  await client.close();
+}
+run();
